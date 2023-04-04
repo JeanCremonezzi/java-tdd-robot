3bf2626f681de2d21beed0745750598682210cea
JeanCremonezzi <jeancremonezzidev@gmail.com>
2023-04-04 02:42:25 -0300

    feat: Added Coordinate class and Robot dummy implementation

---
+++ src/main/java/br/edu/ifsp/scl/Coordinate.java
@@ -0,0 +1,34 @@
+package br.edu.ifsp.scl;
+
+import java.util.Objects;
+
+public class Coordinate {
+    private final int xAxis;
+    private final int yAxis;
+
+    public Coordinate(int xAxis, int yAxis) {
+        this.xAxis = xAxis;
+        this.yAxis = yAxis;
+    }
+
+    public int getxAxis() {
+        return xAxis;
+    }
+
+    public int getyAxis() {
+        return yAxis;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Coordinate that = (Coordinate) o;
+        return xAxis == that.xAxis && yAxis == that.yAxis;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(xAxis, yAxis);
+    }
+}
--- src/main/java/br/edu/ifsp/scl/Robot.java
+++ src/main/java/br/edu/ifsp/scl/Robot.java
@@ -1,6 +1,15 @@
 package br.edu.ifsp.scl;
 
 public class Robot {
-    public Robot() {
+    private Coordinate position;
+    private int orientation;
+
+    public Robot(Coordinate position, int orientation) {
+        this.position = position;
+        this.orientation = orientation;
+    }
+
+    public Coordinate checkObstaclePosition(int beam, int distance) {
+        return new Coordinate(0, 0);
     }
 }
