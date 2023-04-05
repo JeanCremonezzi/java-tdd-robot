df946b440440d37df0baee329ab8537a745b19c6
JeanCremonezzi <jeancremonezzidev@gmail.com>
2023-04-04 21:47:43 -0300

    feat: Main with user input

--- src/main/java/br/edu/ifsp/scl/Main.java
+++ src/main/java/br/edu/ifsp/scl/Main.java
@@ -1,7 +1,31 @@
 package br.edu.ifsp.scl;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.println("Robot X axis: ");
+        final int robotxAxis = userInput.nextInt();
+
+        System.out.println("Robot y axis: ");
+        final int robotyAxis = userInput.nextInt();
+
+        System.out.println("Robot orientation: ");
+        final int robotOrientation = userInput.nextInt();
+
+        System.out.println("Sensor beam: ");
+        final int beam = userInput.nextInt();
+
+        System.out.println("Sensor distance: ");
+        final int distance = userInput.nextInt();
+
+        Coordinate robotPosition = new Coordinate(robotxAxis, robotyAxis);
+        Robot robot = new Robot(robotPosition, robotOrientation);
+
+        Coordinate obstaclePosition = robot.checkObstaclePosition(beam, distance);
+
+        System.out.println("Obstacle position is " + obstaclePosition.toString());
     }
 }
